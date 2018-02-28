040f41087fc1d70c36a4fa6409d4405fc8f88a59
sschmidt <ss@novel-t.ch>
2018-02-28 13:20:10 +0100

    Code clean up + home finalization with Home data JSON implementation

--- herams/js/home/directives/homemap.js
+++ herams/js/home/directives/homemap.js
@@ -1,15 +1,3 @@
-/*
-"use strict";
-angular.module('app-herams').controller('MapCtrl', function(MainMapSvc,$log) {
-
-    var Esri_WorldGrayCanvas = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}';
-    var mainMap = MainMapSvc.createMainMap('mapid',34.553,18.048,Esri_WorldGrayCanvas);
-
-    MainMapSvc.addLayerToMainMap(mainMap,'config/geojson_Admin0.json',9.0820,8.6753);
-
-});
-*/
-
 'use strict';
 
 /**
@@ -38,7 +26,12 @@ angular.module('app-herams').directive('homemap', function(MainMapSvc,$timeout,$
             $timeout(function() {
 
                 /* create Map */
-                var mainMap = MainMapSvc.createMainMap('mapid',$scope.mapdata.map_lat,$scope.mapdata.map_long,$scope.mapdata.map_baselayer);
+                var mainMap = MainMapSvc.createMainMap(
+                    'mapid',
+                    $scope.mapdata.map_lat,
+                    $scope.mapdata.map_long,
+                    $scope.mapdata.map_baselayer
+                );
 
                 /* adding Nigeria */
                 var countries = $scope.mapdata.countries;
