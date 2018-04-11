19a47928a33f51b444c38bc48ca51fd5bec920e7
sophienovelt <ss@novel-t.ch>
2018-04-11 12:56:51 +0200

    Overview - Table review + map markers implementation start

--- herams/js/overview/services/HFMapSvc.js
+++ herams/js/overview/services/HFMapSvc.js
@@ -6,9 +6,57 @@
  * @description
  *   This service provides a set of methods to handle leaflet on the main page
  */
-angular.module('app-herams').service('HFMapSvc', function($rootScope,$state,$timeout,$window,$compile,$log,esriSvc) {
+angular.module('app-herams').service('HFMapSvc', function($rootScope,$state,$timeout,$window,$compile,$log,esriSvc,commonSvc) {
 
     var map;
+
+    function displayData() {
+        return commonSvc.loadData('config/overview_map_sample.json').then(loadSuccess)
+                    .catch(loadFailure)
+                    .then(loadFinally);
+
+        function loadSuccess(httpResponse) {
+
+            var rslts = httpResponse.data.results;
+
+            $log.info('loaded Overview Map Data correctly: ',rslts);
+        }
+
+        function loadFailure(httpResponse) {
+            $log.info('There has been an error Overview Map Data');
+        }
+
+        function loadFinally(httpResponse) {
+            $log.info('Overview Map - last but not least');
+        }
+    }
+
+    /*
+    $scope.init = function() {
+
+        return commonSvc.loadData('config/home_data.json').then(loadSuccess)
+                    .catch(loadFailure)
+                    .then(loadFinally);
+
+        function loadSuccess(httpResponse) {
+
+            $scope.homedata = httpResponse.data.results;
+            setCollapse();
+
+            $log.info('loaded Home Data correctly: ',httpResponse);
+        }
+
+        function loadFailure(httpResponse) {
+            $log.info('There has been an error Home Data');
+        }
+
+        function loadFinally(httpResponse) {
+            $log.info('Home - last but not least');
+        }
+
+    }
+
+     */
 
     return {
 
@@ -30,6 +78,9 @@ angular.module('app-herams').service('HFMapSvc', function($rootScope,$state,$tim
             for (var i in mapdata.basemaps) {
                var tmp = L.tileLayer(mapdata.basemaps[i]).addTo(map);
             }
+
+
+            displayData();
 
             /* - adding ESRI layer - */
             esriSvc.getEsriShape(map, mapdata.layers[0]);
