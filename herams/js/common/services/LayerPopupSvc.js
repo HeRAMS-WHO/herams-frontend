4a23677baa0d19a061845b9a3db76006005f0625
sophienovelt <ss@novel-t.ch>
2018-04-23 13:56:09 +0200

    Reactivate mouseout 4 Homepage popups

--- herams/js/common/services/LayerPopupSvc.js
+++ herams/js/common/services/LayerPopupSvc.js
@@ -24,14 +24,12 @@ angular.module('app-herams').service('LayerPopupSvc', function($timeout,$compile
                 }
             });
 
-/*
             maplayer.on('mouseout', function(e) {
                 if ($('.entry-popup.layer'+cnt).length>0) {
                     $('.entry-popup.layer'+cnt).remove();
                 }
             });
 
-*/
             maplayer.on('click', function(e) {
                 commonSvc.gotoProject();
             });
